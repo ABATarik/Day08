a14ded721e8476cadc1622d92b9b5c8f5037ef10
ABATarik <abaspahic.tarik@outlook.com>
2021-03-15 21:58:15 +0100

    John & Michael

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function functionpPrintInfo(name,age){
+
+
+
+    return(name + age)
+}
+
+console.log (functionpPrintInfo ("\"John\" ", 23))
+console.log (functionpPrintInfo("\"Michael\" ", 43))
+                                
+
+module.exports = functionpPrintInfo;
