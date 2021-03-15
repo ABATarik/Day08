74de0ecdcaf21b8e438a5110fdea4cd8fc37d004
ABATarik <abaspahic.tarik@outlook.com>
2021-03-16 00:33:46 +0100

    ne radi

---
+++ ex07/src/index.js
@@ -0,0 +1,17 @@
+var sum = 0;
+function addThree(){
+    sum = sum +3;
+}
+var sum = 0;
+function addFive(){
+    sum = sum + 5;
+}
+var returnedValue= addFive(8);
+
+module.exports = {
+ 
+    addThree,
+ 
+    addFive
+
+};
