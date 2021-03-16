516053a7ce0cb87c427da07a5b1b5b32dc3bc3f9
ABATarik <abaspahic.tarik@outlook.com>
2021-03-16 02:42:53 +0100

    StrToInt

---
+++ ex14/src/index.js
@@ -0,0 +1,9 @@
+function convertStrToInt(str){
+var str = parseInt(str);
+
+return str 
+};
+console.log(convertStrToInt("44"));
+console.log(convertStrToInt("27"));
+console.log(convertStrToInt("JaffBezos"));
+module.exports=convertStrToInt;
